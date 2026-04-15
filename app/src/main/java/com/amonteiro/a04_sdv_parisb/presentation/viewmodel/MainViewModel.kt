5564903d2008e27e8fe096c30e67d764bc190b16
anth0 <anth06ny@gmail.com>
2026-04-15 12:15:51 +0200

    Fin TP Exception

--- app/src/main/java/com/amonteiro/a04_sdv_parisb/presentation/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/amonteiro/a04_sdv_parisb/presentation/viewmodel/MainViewModel.kt
@@ -7,19 +7,21 @@ import com.amonteiro.a04_sdv_parisb.data.remote.WeatherEntity
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 suspend fun main() {
     val viewModel = MainViewModel()
-    viewModel.loadWeathers("Nice")
+    viewModel.loadWeathers("")
 
-    while(viewModel.runInProgress.value) {
+    while (viewModel.runInProgress.value) {
         println("Attente...")
         delay(500)
     }
 
     //Affichage de la liste (qui doit être remplie) contenue dans la donnée observable
     println("List : ${viewModel.dataList.value}")
+    println("errorMessage : ${viewModel.errorMessage.value}")
 
     //Pour que le programme s'arrête, inutile sur Android
     KtorWeatherApi.close()
@@ -29,18 +31,26 @@ class MainViewModel : ViewModel() {
     //MutableStateFlow est une donnée observable
     val dataList = MutableStateFlow(emptyList<WeatherEntity>())
     val runInProgress = MutableStateFlow(false)
+    val errorMessage = MutableStateFlow("")
 
     fun loadWeathers(cityName: String) {
 
         runInProgress.value = true
+        errorMessage.value = ""
+
 
         viewModelScope.launch(Dispatchers.IO) {
-            //dataList.value = KtorWeatherApi.loadWeathers(cityName)
-            dataList.value = KtorWeatherApi.loadWeathersFake(cityName)
-
-            runInProgress.value = false
+            try {
+                dataList.value = KtorWeatherApi.loadWeathers(cityName)
+                //dataList.value = KtorWeatherApi.loadWeathersFake(cityName)
+            }
+            catch (e: Exception) {
+                e.printStackTrace()
+                errorMessage.value = e.message ?: "Une erreur est survenue"
+            }
+            finally {
+                runInProgress.value = false
+            }
         }
-
-
     }
 }
