ef4374f3553731d5163ed8029d4ec5ad9b69e490
anth0 <anth06ny@gmail.com>
2026-04-15 16:30:19 +0200

    TP Compose Donnée

--- app/src/main/java/com/amonteiro/a04_sdv_parisb/presentation/ui/screens/SearchScreen.kt
+++ app/src/main/java/com/amonteiro/a04_sdv_parisb/presentation/ui/screens/SearchScreen.kt
@@ -1,5 +1,6 @@
 package com.amonteiro.a04_sdv_parisb.presentation.ui.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -10,10 +11,13 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.amonteiro.a04_sdv_parisb.presentation.ui.theme.A04_sdv_parisbTheme
+import com.amonteiro.a04_sdv_parisb.presentation.viewmodel.MainViewModel
 
 @Preview(showBackground = true, showSystemUi = true)
 @Preview(showBackground = true, showSystemUi = true,
@@ -30,16 +34,18 @@ fun SearchScreenPreview() {
 }
 
 @Composable
-fun SearchScreen(modifier: Modifier = Modifier) {
+fun SearchScreen(modifier: Modifier = Modifier, mainViewModel: MainViewModel = MainViewModel()) {
+
+    val list = mainViewModel.dataList.collectAsStateWithLifecycle().value
 
     Column(modifier= modifier) {
         println("SearchScreen()")
         Text(text = "Text1",fontSize = 20.sp)
-        Spacer(Modifier.size(8.dp))
-        Text(text = "Text2",fontSize = 14.sp)
+        Spacer(modifier =Modifier.size(8.dp))
+        Text(text = "Text2",fontSize = 15.sp, modifier=Modifier.background(Color.Red))
 
-        repeat(5) {
-            PictureRowItem(text = "coucou$it")
+        repeat(list.size) {
+            PictureRowItem(text = list[it].name)
         }
     }
 
